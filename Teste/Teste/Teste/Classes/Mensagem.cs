e2e7838cab6b6371a1126ca1ed8dd10c2abcb460
Anderson Silva <ander.loirao@gmail.com>
2018-06-25 11:12:24 -0300

    inicial

---
+++ Teste/Teste/Teste/Classes/Mensagem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teste.Classes
+{
+    public class Mensagem
+    {
+        public int Id { get; set; }
+        public string Assunto { get; set; }
+        public string Aviso { get; set; }
+        public string Previa { get => Aviso.Substring(1, 160) + "..."; set => Aviso = value; }
+        public string Tipo { get; set; }
+        public string Visualizado { get; set; }
+        public DateTime DtAviso { get; set; }
+    }
+}
---
+++ Teste/Teste/Teste/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Teste.Classes;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Teste
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            ListView.ItemsSource = new MainPage().Carregar("1103202");
+
+        }
+
+        
+        private List<Mensagem> Carregar(String Matricula)
+        {
+
+            string EnderecoURL = "http://10.0.1.4:8093/datasnap/rest/TSMAcademico/doConsultaAvisoslApp/" + Matricula;
+
+            WebClient wc = new WebClient();
+            NetworkCredential credentials = new NetworkCredential("0", "0");
+            wc.Credentials = credentials;
+
+            String Conteudo = wc.DownloadString(EnderecoURL); // metodo sincrono -- trava tela
+
+
+            return JsonConvert.DeserializeObject<List<Mensagem>>(Conteudo);
+
+        }
+    }
+}
